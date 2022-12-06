6684838ee8f33230c6d960d98c2db8fc43a5a72d
Ekaterina Volkova <78733281+evolkova01@users.noreply.github.com>
2022-12-06 17:11:56 +0300

    update file

--- src/app/script.py
+++ src/app/script.py
@@ -1,10 +1,11 @@
 import sys
 
 if __name__ == "__main__":
-    print ("Привет :)")
+    print ("Привет, студент :)")
     
     mes = int(input("Введите число для возведения в степень: "))
     n = int(input("Введите степень: "))
+    
     mes = mes ** n
   
     print(f"Результат: " + str(mes))
